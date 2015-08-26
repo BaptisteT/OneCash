a5b3a95255bb532fd7d111eeb350696f04b23742
Baptiste <baptiste.truchot@gmail.com>
2015-08-26 09:26:05 +0200

    Initial Commit

---
+++ One/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  One
+//
+//  Created by Baptiste Truchot on 8/26/15.
+//  Copyright (c) 2015 Mindie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ One/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  One
+//
+//  Created by Baptiste Truchot on 8/26/15.
+//  Copyright (c) 2015 Mindie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
