23ee3e01589eca0e509c104f7618a039b7814b09
Clement Raffenoux <clement.raffenoux@gmail.com>
2015-09-30 16:47:54 +0200

    card design + usernameVC design

--- One/UsernameCardView.m
+++ One/UsernameCardView.m
@@ -7,14 +7,60 @@
 //
 
 #import "UsernameCardView.h"
+
+#import "User.h"
 #import "ColorUtils.h"
+
+
+@interface UsernameCardView ()
+@property (strong, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (strong, nonatomic) IBOutlet UILabel *dollarLabel;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (strong, nonatomic) User *currentUser;
+
+@end
 
 @implementation UsernameCardView
 
 - (void)initWithFrame:(CGRect)frame andDelegate:(id<UsernameViewDeletagteProtocol>)delegate {
+    
+    //Init
     [self setFrame:frame];
     self.delegate = delegate;
+    
+    //User
+    self.currentUser = [User currentUser];
+    
+    //UI
     self.backgroundColor = [ColorUtils mainGreen];
+    self.layer.cornerRadius = self.frame.size.height / 80;
+    self.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.layer.shadowOffset = CGSizeMake(0, 0);
+    self.layer.shadowOpacity = 0.1;
+    self.layer.shadowRadius = 20;
+    self.avatarImageView.layer.borderColor = [ColorUtils darkGreen].CGColor;
+    self.avatarImageView.layer.borderWidth = 8;
+    
+    //Avatar
+    [self.currentUser setAvatarInImageView:self.avatarImageView bigSize:YES saveLocally:NO];
+    
+    //Username
+    self.usernameLabel.text = self.currentUser.caseUsername;
+    
+    //Title
+    self.titleLabel.text = NSLocalizedString(@"card_title", nil);
+    
 }
+
+-(void)layoutSubviews {
+    [super layoutSubviews];
+    [self layoutIfNeeded];
+    self.avatarImageView.layer.cornerRadius = self.avatarImageView.frame.size.width / 2;
+    self.avatarImageView.clipsToBounds = YES;
+    self.dollarLabel.layer.cornerRadius = self.dollarLabel.frame.size.width / 2 ;
+    self.dollarLabel.clipsToBounds = YES;
+}
+
 
 @end
--- One/UsernameViewController.m
+++ One/UsernameViewController.m
@@ -13,6 +13,8 @@
 
 @interface UsernameViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *closeButton;
+@property (strong, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (strong, nonatomic) IBOutlet UILabel *topLabel;
 @end
 
 @implementation UsernameViewController
@@ -26,6 +28,35 @@
     
     //Init card
     [self createUsernameCard];
+    
+    //Wording
+    NSString *username = [User currentUser].caseUsername;
+    self.descriptionLabel.text = [NSString stringWithFormat:NSLocalizedString(@"card_description", nil), username];
+    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:self.descriptionLabel.text];
+    NSRange boldRange = [self.descriptionLabel.text rangeOfString:username];
+    UIFont *boldFont = [UIFont fontWithName:@"ProximaNova-Semibold" size:self.descriptionLabel.font.pointSize * 1.2];
+    [attrString addAttribute: NSFontAttributeName value:boldFont range:boldRange];
+    [attrString addAttribute: NSForegroundColorAttributeName value:[UIColor whiteColor] range:boldRange];
+    self.descriptionLabel.attributedText = attrString;
+    self.topLabel.text = NSLocalizedString(@"card_top_title", nil);
+
+    
+    //UI
+    self.closeButton.layer.zPosition = 1;
+    self.descriptionLabel.layer.zPosition = 1;
+    self.topLabel.layer.zPosition = 1;
+    
+    //Animation
+    [self addDollarLabel];
+    [NSTimer scheduledTimerWithTimeInterval:0.5
+                                     target:self
+                                   selector:@selector(addDollarLabel)
+                                   userInfo:nil
+                                    repeats:YES];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
 }
 
 // --------------------------------------------
@@ -47,7 +78,8 @@
     CGRect frame = CGRectMake((self.view.frame.size.width - width) / 2, (self.view.frame.size.height - height) / 2, width, height);
     UsernameCardView *usernameCardView = [[[NSBundle mainBundle] loadNibNamed:@"UsernameCard" owner:self options:nil] objectAtIndex:0];
     [usernameCardView initWithFrame:frame andDelegate:self];
-    [self.view insertSubview:usernameCardView atIndex:0];
+    [self.view addSubview:usernameCardView];
+    usernameCardView.layer.zPosition = 1;
 }
 
 // --------------------------------------------
@@ -62,9 +94,69 @@
 #pragma mark - Actions
 // --------------------------------------------
 
--(User *)currentUser {
-    User *currentUser = [User currentUser];
-    return currentUser;
+- (void)addDollarLabel {
+    int rndX = 30 + arc4random() % ((int)(self.view.frame.size.width - 30) - 30);
+    
+    UILabel *dollarLabel = [[UILabel alloc] initWithFrame:CGRectMake(rndX, -40, 40, 40)];
+    
+    //UI
+    dollarLabel.text = @"$";
+    dollarLabel.textAlignment = NSTextAlignmentCenter;
+    dollarLabel.backgroundColor = [ColorUtils darkGreen];
+    dollarLabel.textColor = [UIColor whiteColor];
+    dollarLabel.font = [UIFont fontWithName:@"ProximaNova-Semibold" size:20];
+    dollarLabel.transform = CGAffineTransformMakeScale(1, 1);
+    dollarLabel.layer.zPosition = 0;
+    
+    [self.view addSubview:dollarLabel];
+    dollarLabel.clipsToBounds = YES;
+    dollarLabel.layer.cornerRadius = 20;
+    
+    CGFloat duration = 10 + (arc4random() % 5 - 2);
+    [UIView animateWithDuration:0.3 animations:^{
+        dollarLabel.transform = CGAffineTransformMakeScale(1, 1);
+        dollarLabel.transform = CGAffineTransformMakeRotation(-0.01 * (arc4random() % 20));
+    }];
+    [UIView animateWithDuration:duration animations:^{
+        dollarLabel.alpha = 0;
+    }];
+    CAKeyframeAnimation *animation = [self createAnimation:dollarLabel.frame];
+    animation.duration = duration;
+    [dollarLabel.layer addAnimation:animation forKey:@"position"];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)((duration /2) * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [dollarLabel removeFromSuperview];
+    });
 }
+
+- (CAKeyframeAnimation *)createAnimation:(CGRect)frame {
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    int height = self.view.frame.size.height / 1.2 + arc4random() % 40 - 20;
+    int xOffset = frame.origin.x;
+    int yOffset = frame.origin.y;
+    int waveWidth = 50;
+    CGPoint p1 = CGPointMake(xOffset, height * 0 + yOffset);
+    CGPoint p2 = CGPointMake(xOffset, height * 1 + yOffset);
+    CGPoint p3 = CGPointMake(xOffset, height * 2 + yOffset);
+    CGPoint p4 = CGPointMake(xOffset, height * 2 + yOffset);
+    
+    CGPathMoveToPoint(path, NULL, p1.x,p1.y);
+    
+    if (arc4random() % 2) {
+        CGPathAddQuadCurveToPoint(path, NULL, p1.x - arc4random() % waveWidth, p1.y + height / 2.0, p2.x, p2.y);
+        CGPathAddQuadCurveToPoint(path, NULL, p2.x + arc4random() % waveWidth, p2.y + height / 2.0, p3.x, p3.y);
+        CGPathAddQuadCurveToPoint(path, NULL, p3.x - arc4random() % waveWidth, p3.y + height / 2.0, p4.x, p4.y);
+    } else {
+        CGPathAddQuadCurveToPoint(path, NULL, p1.x + arc4random() % waveWidth, p1.y + height / 2.0, p2.x, p2.y);
+        CGPathAddQuadCurveToPoint(path, NULL, p2.x - arc4random() % waveWidth, p2.y + height / 2.0, p3.x, p3.y);
+        CGPathAddQuadCurveToPoint(path, NULL, p3.x + arc4random() % waveWidth, p3.y + height / 2.0, p4.x, p4.y);
+    }
+    animation.path = path;
+    animation.calculationMode = kCAAnimationCubicPaced;
+    CGPathRelease(path);
+    return animation;
+}
+
 
 @end
